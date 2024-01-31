bc71717deec47afc2e76a998c7cdd87e9aa1b6f9
Aunpun283 <151039011+Aunpun283@users.noreply.github.com>
2024-01-31 21:05:52 +0700

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fv$&am@1^nkarlpr_oxo2sf-s9o+k&*hq%zul1hmk#1g=&k7ud'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
